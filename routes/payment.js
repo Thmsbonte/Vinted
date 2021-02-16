23a021238f9487ec5b90d511b8d958ae2c9937d5
Thmsbonte <thms.bonte@gmail.com>
2021-02-17 00:25:20 +0100

    add delete offer request in payment route

--- routes/payment.js
+++ routes/payment.js
@@ -18,19 +18,27 @@ router.post("/payment", async (req, res) => {
 
   try {
     if (offer_id) {
-      // If we have a offer id, request to get offer information from DB
+      // If we have an offer id, request to get offer information from DB
       const offer = await Offer.findById(offer_id);
       const price = Number(offer.product_price) * 100;
       try {
-        // Request to do the payment
+        // Payment request
         const response = await stripe.charges.create({
           amount: price,
           currency: "eur",
           description: offer.product_description,
           source: stripeToken,
         });
-
         res.status(200).json(response);
+        // If OK delete of the offer in the DB
+        try {
+          const deleteOffer = await Offer.findByIdAndDelete(offer_id).populate(
+            "owner"
+          ); // We also get the owner's offer to notify him
+        } catch (error) {
+          // If delete fails we have to generate an offer in the back-office (to do)
+          console.log(error.message);
+        }
       } catch (error) {
         console.log(error.message);
         res.status(400).json({ message: "Payment failed, please try again" });
